3427854f80045db47751cb879fcf34d9de20b78f
Alfadul Alawi Albar <ajt1247@AJT.COM.SA>
2025-06-19 11:11:08 +0300

    Final commit: Android v2 embedding version

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text('Flutter ID Scanner - Final v2 Ready'),
+      ),
+    ),
+  ));
+}
